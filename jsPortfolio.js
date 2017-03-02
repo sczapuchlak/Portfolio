34f676743f1a300e99a9c38321cc5c4da3b5cb27
sczapuchlak <[sczapuchlak@gmail.com]>
2017-03-02 02:16:31 -0600

    missing a little JavaScript but the finished product

---
+++ jsPortfolio.js
@@ -0,0 +1,20 @@
+
+function upperCase() {
+    var x = document.getElementById("name");
+    x.value = x.value.toUpperCase();}
+
+function mOver(obj) {
+    obj.innerHTML = "Being yourself is."
+}
+
+function mOut(obj) {
+    obj.innerHTML = "Standing out is."
+}
+
+function mOverDesign(obj) {
+    obj.innerHTML = "Web Designer"
+}
+
+    function mOutDesign(obj) {
+        obj.innerHTML = "Web Developer"
+    }
