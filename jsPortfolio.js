960360b72df0f965ec8889f17cf0a69e6c711c26
sczapuchlak <[sczapuchlak@gmail.com]>
2017-03-02 02:26:59 -0600

    JavaScript Complete

--- jsPortfolio.js
+++ jsPortfolio.js
@@ -42,6 +42,6 @@ var x = setInterval(function() {
     // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("demo").innerHTML = "Soon! I Promise!";
     }
 }, 1000);
